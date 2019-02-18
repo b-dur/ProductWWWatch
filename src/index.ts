0f0b8a8fc1609848fffadc34e75a743743215ead
Bardur Sigmundarson <b-dur@b-dur.dk>
2019-02-18 12:46:21 +0100

    Add new section and publish watchers to unordered list

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 import { getCode } from "./utils";
 
-const url = `https://productwatch.azurewebsites.net/api/api?code=${getCode()}&name=234`;
-fetch(url);
+async function loadContent() {
+  const url = `https://productwatch.azurewebsites.net/api/api?code=${getCode()}`;
+  const resp = await fetch(url);
+  const data = await resp.json();
+  document.getElementById("watcherlist").innerHTML = data.map(
+    x =>
+      `<li>Created at: ${new Date(x.createdAt)} <br/> Trigger price: ${
+        x.triggerPrice
+      } <br/> Url: ${x.url}</li>`
+  );
+}
+loadContent();
