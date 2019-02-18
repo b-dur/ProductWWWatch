f1c267f360127e410d4d6bd2b1e86d0e688d9cdb
Bardur Sigmundarson <b-dur@b-dur.dk>
2019-02-18 14:14:05 +0100

    Create IWatcher type

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import { getCode, getFormValue } from "./utils";
 
+interface IWatcher {
+  createdAt: number;
+  triggerPrice: number;
+  url: string;
+}
+
 async function loadContent() {
   const url = `https://productwatch.azurewebsites.net/api/api?code=${getCode()}`;
   const resp = await fetch(url);
-  const data = await resp.json();
-  document.getElementById("watcherlist").innerHTML = data.map(
-    x =>
-      `<li>Created at: ${new Date(x.createdAt)} <br/> Trigger price: ${
-        x.triggerPrice
-      } <br/> Url: ${x.url}</li>`
-  );
+  const data: IWatcher[] = await resp.json();
+  if (data.length) {
+    document.getElementById("watcherlist").innerHTML = data
+      .map(
+        x =>
+          `<li>Created at: ${new Date(x.createdAt)} <br/> Trigger price: ${
+            x.triggerPrice
+          } <br/> Url: ${x.url}</li>`
+      )
+      .join();
+  } else {
+    document.getElementById("watcherlist").innerHTML = `<li>No data found</li>`;
+  }
 }
 loadContent();
 
@@ -17,18 +29,18 @@ document.forms
   .namedItem("addWatcher")
   .addEventListener("submit", (e: Event) => {
     e.preventDefault();
-    const formValues = getFormValue(e.target as HTMLFormElement);
+    const formValues = getFormValue(e.target as HTMLFormElement) as IWatcher;
     formValues.createdAt = Date.now();
 
     AddWatcher(formValues);
     console.log("TCL: formValues", formValues);
   });
 
-export const AddWatcher = async (data: any) => {
+export const AddWatcher = async (watcher: IWatcher) => {
   const url = `https://productwatch.azurewebsites.net/api/api?code=${getCode()}`;
   const resp = await fetch(url, {
     method: "POST",
-    body: JSON.stringify(data),
+    body: JSON.stringify(watcher),
     headers: {
       "Content-Type": "application/json"
     }
